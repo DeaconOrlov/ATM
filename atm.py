5063bb61c84d0705e34c7343aa6b3098c01275d6
DeaconOrlov <deaconorlov@gmail.com>
2021-10-31 14:15:04 -0400

    edit withdraw, deposit, functions.  minimally function complete

--- atm.py
+++ atm.py
@@ -7,22 +7,18 @@ selectedPin = int(input("Enter PIN: "))
 
 #Extracts the row with the pin from the data frame
 acctIndex = df[df['pin']==selectedPin].index.values
-selectedAcct = df.iloc[acctIndex,]
 
-"""
 # Add to balance and return new balance
-def make_Deposit():
-        depositAmt = float(input("How much would you like to deposit? "))
-        global balance
-        balance +=  depositAmt
-        print("You deposited {} dollars. Your new balance is: {} ".format(depositAmt, balance))
-"""
+def deposit_Amt():
+    depositAmt = int((input("How much would you like to deposit? ")))
+    df.loc[acctIndex, 'balance'] = df.loc[acctIndex, 'balance'] + depositAmt
+    
 
 # Withdraw from balance, checks if balance is greater than zero, returns new balance
 def withdraw_Amt():
     withdrawAmt = int((input("How much would you like to withdraw? ")))
-    df.at[acctIndex, 'balance'] = df.at[acctIndex, 'balance'] - withdrawAmt
-    
+    df.loc[acctIndex, 'balance'] = df.loc[acctIndex, 'balance'] - withdrawAmt
+           
 """
     if withBal > 0:
             print("You withdrew {} dollars. Your remaining balance is: {} dollars".format(withdrawAmt, withBal))
@@ -32,8 +28,7 @@ def withdraw_Amt():
 """
 # Formats and prints balance from data frame slice determined by selectedPin
 def check_balance():
-    curBal = selectedAcct.at[int(acctIndex), 'balance']
-    curBalF = "${:,.2f}".format(curBal)
+    curBalF = "${:,.2f}".format(df.at[int(acctIndex), 'balance'])
     print(f"Your current balance is {curBalF}")
     
      
@@ -47,32 +42,32 @@ else:
 
 while loggedIn == 1:
     # Print menu options
+    print(f"Hello, what would you like to do today?")
     print("1 - Check Balance")
     print("2 - withdraw")
-    print("3 - End Session")
-    print("4 - Deposit")
-    print(type(selectedAcct), type(acctIndex))
+    print("3 - Deposit")
+    print("4 - End Session")
     # Get selection Input
     selection = input("Enter Selection: ")
 
     # Selection 1
     if selection == "1":
         check_balance()
-    
-        
+            
     # Selection 2
     elif selection == "2":
         withdraw_Amt()
-        
-    # Selection 3
-    elif selection == "3":
+            
+    elif selection  == "3":
+        deposit_Amt()
+                
+    # Selection 4
+    elif selection == "4":
         print("logging out")
+        df.to_csv(('atm/accounts.csv'))
 
         # Log out user (exit loop)
         loggedIn = 0
-    
-    elif selection  == "4":
-        make_Deposit()
-        
+
     else:
         print("Invalid selection")
