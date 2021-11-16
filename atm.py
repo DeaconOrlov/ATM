efbab40ba77b6a4434c2f619aa9b2ee5534a6e7c
DeaconOrlov <deaconorlov@gmail.com>
2021-11-16 18:54:31 -0500

    add logic to confirm pin is in dataframe and prompt user to re-enter valid pin if not

--- atm.py
+++ atm.py
@@ -1,14 +1,16 @@
-from typing import ValuesView
 import pandas as pd
 import numpy as np
 df = pd.read_csv('accounts.csv')
+
+loggedIn = 0
 
 # Formats and prints balance from data frame slice determined by selectedPin
 def check_balance():
     curBalF = "${:,.2f}".format(df.at[int(acctIndex), 'balance'])
     print(f"Your current balance is {curBalF}") 
 
-# Withdraw from balance, checks if balance is greater than zero, returns new balance
+# Withdraw from balance, if balance is less than 0 return insufficient funds 
+# and restore balance, else print new balance
 def withdraw_Amt():
     withdrawAmt = int((input("How much would you like to withdraw? ")))
     df.loc[acctIndex, 'balance'] = df.loc[acctIndex, 'balance'] - withdrawAmt  
@@ -30,11 +32,11 @@ selectedPin = int(input("Enter PIN: "))
 acctIndex = df[df['pin']==selectedPin].index.values
 
 # Set Logged in value to 1 now user is logged in
-# Checks pin input against saved pin
 if selectedPin in df.values:
     loggedIn = 1
 else:
-    loggedIn = 0
+    print('Invalid PIN, please enter a valid PIN')
+    selectedPin = int(input("Enter PIN: "))
     
 while loggedIn == 1:
     # Print menu options
